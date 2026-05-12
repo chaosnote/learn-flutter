c5ea1383d6ada42fd28270e66b5bd07a7deda2f6
chris <yeditor@gmail.com>
2026-05-12 14:11:36 +0800

    調整變數使用

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -39,12 +39,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
+          Text(pair.asLowerCase),
           ElevatedButton(
             onPressed: () {
               console.log('button pressed!'); // 輸出至 VSCode Debug Console
