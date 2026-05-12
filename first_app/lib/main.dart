6582ae6e97e18c95a1042c834918147c5e277991
chris <yeditor@gmail.com>
2026-05-12 16:14:25 +0800

    學習 Widget Inspector

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -43,6 +43,7 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('A random AWESOME idea:'),
           BigCard(pair: pair),
@@ -68,10 +69,20 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Text(pair.asLowerCase),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
